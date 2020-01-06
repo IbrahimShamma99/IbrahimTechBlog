d567560f15c1134b91f1039d455559341755c3d0
Ibrahim shamma <i.abushammah@gmail.com>
2020-01-06 20:38:55 +0200

    init server

--- server.js
+++ server.js
@@ -1,1 +1,6 @@
-var
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
