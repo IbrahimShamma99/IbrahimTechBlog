5ebea5627f6349df9ebea5f292a23e62f078c6ec
Ibrahim shamma <i.abushammah@gmail.com>
2020-01-07 18:09:21 +0200

    cleaning

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,20 +1,20 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
+import {Link} from 'gatsby';
 
-import { rhythm, scale } from "../utils/typography"
+import {rhythm, scale} from '../utils/typography';
 
 class Layout extends React.Component {
-  render() {
-    const { location, title, children } = this.props
-    const rootPath = `${__PATH_PREFIX__}/`
-    let header
+  render () {
+    const {location, title, children} = this.props;
+    const rootPath = `${__PATH_PREFIX__}/`;
+    let header;
 
     if (location.pathname === rootPath) {
       header = (
         <h1
           style={{
-            ...scale(1.5),
-            marginBottom: rhythm(1.5),
+            ...scale (1.5),
+            marginBottom: rhythm (1.5),
             marginTop: 0,
           }}
         >
@@ -26,10 +26,10 @@ class Layout extends React.Component {
             }}
             to={`/`}
           >
-            {title}
-          </Link>
+            {' '}{title}{' '}
+          </Link>{' '}
         </h1>
-      )
+      );
     } else {
       header = (
         <h3
@@ -46,30 +46,39 @@ class Layout extends React.Component {
             }}
             to={`/`}
           >
-            {title}
-          </Link>
+            {' '}{title}{' '}
+          </Link>{' '}
         </h3>
-      )
+      );
     }
     return (
       <div
         style={{
           marginLeft: `auto`,
           marginRight: `auto`,
-          maxWidth: rhythm(24),
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+          maxWidth: rhythm (24),
+          padding: `${rhythm (1.5)} ${rhythm (3 / 4)}`,
         }}
       >
-        <header>{header}</header>
-        <main>{children}</main>
+        <header> {header} </header>
+        {' '}
+        <main> {children} </main>
+        {' '}
         <footer>
-          © {new Date().getFullYear()}, Built with
+          {' '}
+          ©
+          {new Date ().getFullYear ()}
+          , Built by
+          {' '}
           {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+          {' '}
+          <a href="https://ibrahim-app-portfolio.herokuapp.com/"> Gatsby </a>
+          {' '}
         </footer>
+        {' '}
       </div>
-    )
+    );
   }
 }
 
-export default Layout
+export default Layout;
