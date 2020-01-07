e8666787f83534ff877a42fd05d4ec8832665fcc
Ibrahim shamma <i.abushammah@gmail.com>
2020-01-07 11:29:20 +0200

    next added

---
+++ pages/index.js
@@ -0,0 +1,6 @@
+const Index = () => (
+    <div>
+      <p>Hello World it is Ibrahim</p>
+    </div>
+  )
+  export default Index ;
--- server.js
+++ server.js
@@ -1,29 +1,15 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-var app = express();
 var session = require('express-session');
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 var errorhandler = require('errorhandler');
 var config = require("./config/config");
 var cors = require("cors");
+const next = require('next');
 var isProduction = process.env.NODE_ENV === 'production';
-//NOTE Import DB
-require('./models/User');
-require('./models/Article');
-app.use(require('./routes'));
-
-app.use(errorhandler());
-app.use(require('method-override')());
-app.use(cors());
-app.use(session({
-    secret: 'IbrahimBlog',
-    cookie: { maxAge: 60000 },
-    resave: false,
-    saveUninitialized: false
-}));
-
+const dev = process.env.NODE_ENV !== 'production';
+const PORT = require("./config/config.json").PORT;
+const NextApp = next({ dev });
 if (isProduction) {
     mongoose
         .connect(process.env.MONGO_URI, {
@@ -41,26 +27,48 @@ if (isProduction) {
     mongoose.set('debug', true);
 };
 
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.json({
-        'errors': {
-            message: err.message,
-            error: {}
-        }
-    });
-});
+NextApp.prepare()
+    .then(() => {
+        const app = express();
+        app.use(bodyParser.urlencoded({ extended: false }));
+        app.use(bodyParser.json());
+        //NOTE Import DB
+        require('./models/User');
+        require('./models/Article');
+        app.use(require('./routes'));
+        app.use(errorhandler());
+        app.use(require('method-override')());
+        app.use(cors());
+        app.use(session({
+            secret: 'IbrahimBlog',
+            cookie: { maxAge: 60000 },
+            resave: false,
+            saveUninitialized: false
+        }));
+        //NOTE Import DB
+        require('./models/User');
+        require('./models/Article');
+        app.use(require('./routes'));
 
-app.use(function(err, req, res, next) {
-    // if (err.status === undefined) {
-    //     err.status = 422;
-    // };
-    res.status(err.status || 500);
-    res.json({
-        'errors': {
-            message: err.message,
-            error: {}
-        }
+        app.use(function(err, req, res, next) {
+            res.status(err.status || 500);
+            res.json({
+                'errors': {
+                    message: err.message,
+                    error: {}
+                }
+            });
+        });
+        app.use(function(err, req, res, next) {
+            res.status(err.status || 500);
+            res.json({
+                'errors': {
+                    message: err.message,
+                    error: {}
+                }
+            });
+        });
+        var server = app.listen(process.env.PORT || PORT, function() {
+            console.log('Listening on port ' + server.address().port);
+        });
     });
-});
-module.exports = app;
