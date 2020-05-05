39a09bf00adb9993de7b4e49f0385718ac162596
ibrahimshamma99 <i.abushammah@gmail.com>
2020-05-05 15:42:51 +0300

    init dark theme

---
+++ src/components/Footer.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+import { rhythm } from '../utils/typography';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer
+        style={{
+          marginTop: rhythm(2.5),
+          paddingTop: rhythm(1),
+        }}
+      >
+        <div style={{ float: 'right' }}>
+          <a href="/rss.xml" target="_blank" rel="noopener noreferrer">
+            rss
+          </a>
+        </div>
+        <a
+          href="https://twitter.com/ibrahimbon5"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          twitter
+        </a>{' '}
+        &bull;{' '}
+        <a
+          href="https://github.com/IbrahimShamma99"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          github
+        </a>{' '}
+        &bull;{' '}
+        <a
+          href="https://dev.to/ibrahimshamma99"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          DEV
+          {" "}
+        </a>
+        &bull;{' '}
+        <a
+        href="https://www.linkedin.com/in/ibrahim-abushamma/"
+        target="_blank"
+        rel="noopener noreferrer"
+        >
+        linkedin
+        </a>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
