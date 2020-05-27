b13065da078a3dd5c9652983fb5cdc16afb97ee2
ibrahimshamma99 <i.abushammah@gmail.com>
2020-05-27 15:43:40 +0300

    👌 IMPROVE:

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -13,7 +13,7 @@ class Bio extends React.Component {
       >
         <img
           src={profilePic}
-          alt={`Dan Abramov`}
+          alt={`Ibrahim Abushammah`}
           style={{
             marginRight: rhythm(1 / 2),
             marginBottom: 0,
